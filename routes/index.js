6b8fdc57d8b5548e1635f80d84747f77e3c1607c
Steven Castroverde <stevencastroverde@Stevens-MacBook-Air.local>
2017-03-15 11:41:20 -0600

    added collections

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,24 @@
 var express = require('express');
 var router = express.Router();
 var Buttons = require('../uiComponents/buttons');
+var Cards = require('../uiComponents/cards');
+var Collections = require('../uiComponents/collections');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.send('Hello from over here');
 });
 
-router.get('/materialize', (req, res) => {
+router.get('/materialize/buttons', (req, res) => {
     res.json(Buttons);
+})
 
+router.get('/materialize/cards', (req,res) => {
+  res.json(Cards);
+})
+
+router.get('/materialize/collections', (req,res) => {
+  res.json(Collections);
 })
 
 module.exports = router;
--- uiComponents/buttons.js
+++ uiComponents/buttons.js
@@ -1,18 +1,17 @@
 module.exports = {
-    raised: {
-        normal: '<a class="waves-effect waves-light btn">button</a>',
-        iconLeft: '<a class="waves-effect waves-light btn"><i class="material-icons left">cloud</i>button</a>',
-        iconRight: '<a class="waves-effect waves-light btn"><i class="material-icons right">cloud</i>button</a>'
-    },
-    floating: {
-        normal: '<a class="btn-floating btn-large waves-effect waves-light red"><i class="material-icons">add</i></a>'
-    },
-    flat: {
-        normal: '<a class="waves-effect waves-teal btn-flat">Button</a>'
-    },
-    submit: {
-        normal: '<button class="btn waves-effect waves-light" type="submit" name="action">Submit<i class="material-icons right">send</i></button>'
-    },
+  raised: {
+    displayName: 'Raised',
+    category: 'Buttons',
+    html: '<a class="waves-effect waves-light btn">button</a>',
+    iconLeft: '<a class="waves-effect waves-light btn"><i class="material-icons left">cloud</i>button</a>',
+    iconRight: '<a class="waves-effect waves-light btn"><i class="material-icons right">cloud</i>button</a>'
+  },
+    floating:'<a class="btn-floating btn-large waves-effect waves-light red"><i class="material-icons">add</i></a>'
+    ,
+    flat: '<a class="waves-effect waves-teal btn-flat">Button</a>'
+    ,
+    submit:'<button class="btn waves-effect waves-light" type="submit" name="action">Submit<i class="material-icons right">send</i></button>'
+    ,
     large: {
         normal: '<a class="waves-effect waves-light btn-large">Button</a>',
         iconLeft: '<a class="waves-effect waves-light btn-large"><i class="material-icons left">cloud</i>button</a>',
