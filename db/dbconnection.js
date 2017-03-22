6ebbb3b678f0167ac14ee4945ca8e8c28afb87d2
Steven <sfcastroverde@iMac.PK5001Z>
2017-03-21 18:18:09 -0600

    added env var

--- db/dbconnection.js
+++ db/dbconnection.js
@@ -3,7 +3,7 @@ const mongoConfig = require('./mongoConfig');
 const config = mongoConfig[enviornment];
 const mongo = require('mongodb');
 const monk = require('monk');
-const mongoConnection = monk(config);
+const mongoConnection = monk(process.env.MONGODB_URI);
 
 
 module.exports = mongoConnection;
